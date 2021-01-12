3906c6c106a2dc9c0ef3fdb5ffec33328f93bede
Danny Gaspar <dannygaspar.dg@gmail.com>
2021-01-12 17:58:47 +0000

    Launch pipeline from root file and improve logging

---
+++ pipeline/run_pipeline.py
@@ -0,0 +1,34 @@
+import logging
+from pipeline.ingest import icd10_mortality_rates_ingest
+
+
+def configure_logger():
+    logger = logging.getLogger('pipeline')
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('main.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def main():
+    logger = configure_logger()
+
+    logger.info("Starting pipeline...")
+    icd10_mortality_rates_ingest.run()
+
+
+if __name__ == "__main__":
+    main()
