fb2c8a4bc5b57ed8a8c4746c8c0cb714cb035b78
Danny Gaspar <dannygaspar.dg@gmail.com>
2021-01-13 22:47:12 +0000

    Write DataFrames to postgres database

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,5 +1,5 @@
 import logging
-import psycopg2
+from sqlalchemy import create_engine
 from pipeline.ingest import icd10_mortality_rates_processor
 
 
@@ -28,11 +28,15 @@ def main():
     logger = configure_logger()
 
     logger.info("Starting pipeline...")
+
+    engine = create_engine('postgresql://postgres@localhost:37780/who')
+    target_table = 'mortality_rates'
     icd10_mortality_rates_urls = ["https://www.who.int/healthinfo/statistics/Morticd10_part1.zip",
                                   "https://www.who.int/healthinfo/statistics/Morticd10_part2.zip"]
     for url in icd10_mortality_rates_urls:
         processed_icd10_data = icd10_mortality_rates_processor.run(url)
-        # write to database
+        logger.info(f"Writing data to {target_table} table...")
+        processed_icd10_data.to_sql(target_table, con=engine, if_exists='append', index=False, method='multi')
 
 
 if __name__ == "__main__":
