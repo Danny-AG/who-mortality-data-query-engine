71dbd8ae14731d1f3e2680bd2d847d640e3e3c2f
Danny Gaspar <dannygaspar.dg@gmail.com>
2021-01-20 21:59:29 +0000

    Add mock cause and country code test input files

--- definitions.py
+++ definitions.py
@@ -6,3 +6,5 @@ RESOURCES_DIR = ROOT_DIR + "/resources"
 ICD_10_CAUSE_CODES_PATH = RESOURCES_DIR + r"/ICD10_list_101_103_cause_codes.csv"
 ICD_10_PORTUGAL_CAUSE_CODES_PATH = RESOURCES_DIR + r"/ICD10_list_UE1_cause_codes.csv"
 COUNTRY_CODES_PATH = RESOURCES_DIR + "/country_codes/country_codes"
+
+TEST_RESOURCES = RESOURCES_DIR + '/test_files'
